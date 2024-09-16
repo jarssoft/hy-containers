7f7af41e99e776604aa660fc5b7b21a6b9ffc938
Jari Saari <jari.m.saari@gmail.com>
2024-09-16 17:12:59 +0300

    add test

---
+++ todo-app/todo-frontend/src/Todos/Todo.test.jsx
@@ -0,0 +1,14 @@
+import { render, screen } from '@testing-library/react'
+import Todo from './Todo'
+
+test('renders content', () => {
+  const todo = {
+    text: 'Make something',
+    important: true
+  }
+
+  render(<Todo todo={todo} />)
+
+  const element = screen.getByText('Make something')
+  expect(element).toBeDefined() 
+})
